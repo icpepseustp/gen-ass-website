bfa9196308a884f1abc9294e05c32150f8cf3b5d
Justin John Bonita <i.am.justinbonita@gmail.com>
2022-11-16 01:45:45 +0800

    feat: footer & classrep components

---
+++ src/assets/images_reps.js
@@ -0,0 +1,37 @@
+import A1 from "../assets/images/classreps/1a.png"
+import B1 from "../assets/images/classreps/1b.jpg"
+import C1 from "../assets/images/classreps/1c.JPG"
+import D1 from "../assets/images/classreps/1d.jpg"
+import E1 from "../assets/images/classreps/1e.jpg"
+import F1 from "../assets/images/classreps/1f.jpg"
+
+import A2 from "../assets/images/classreps/2a.jpg"
+import B2 from "../assets/images/classreps/2b.jpg"
+import C2 from "../assets/images/classreps/2c.png"
+import D2 from "../assets/images/classreps/2d.jpeg"
+
+import A3 from "../assets/images/classreps/3a.jpg"
+import B3 from "../assets/images/classreps/3b.jpg"
+import C3 from "../assets/images/classreps/3c.jpg"
+
+import A4 from "../assets/images/classreps/4a.jpg"
+import B4 from "../assets/images/classreps/4b.jpg"
+import C4 from "../assets/images/classreps/4c.jpg"
+
+
+const reps = [
+    {
+        "year-1": [A1, B1, C1, D1, E1, F1]
+    },
+    {
+        "year-2": [A2, B2, C2, D2]
+    },
+    {
+        "year-3": [A3, B3, C3]
+    },
+    {
+        "year-4": [A4, B4, C4]
+    },
+]
+
+export default reps
