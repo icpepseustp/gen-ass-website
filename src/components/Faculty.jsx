534230002d16617ac312fbbb205eaf632d771312
diffident016 <marvin.tagolimotjr13@gmail.com>
2022-11-16 06:53:52 +0800

    feat: partial faculty component

--- src/components/Faculty.jsx
+++ src/components/Faculty.jsx
@@ -14,21 +14,32 @@ export default function Faculty() {
       {/* <h2 className='text-[#00EBFE] mt-12 lg:mt-24 text-2xl lg:text-4xl'>THE EXECUTIVES</h2> */}
 
       <div className='flex flex-col w-full p-8 lg:p-20'>
-        <div className='grid grid-cols-2 lg:grid-cols-4 font-poppins gap-y-10 '>
-        {images.map((img, i) => {
-            return(
-                <>
 
-                {i !== 0 && (<div key={img} className='flex flex-col text-center items-center gap-2'>
-                    <img  className='w-24 h-24 lg:w-40 lg:h-40 object-cover rounded-full' src={img} alt='no'/>
-                    <p className='font-medium text-xs lg:text-lg'>{data[i]["name"]}</p>
+        <div className='grid grid-cols-1 font-poppins mb-10'>
+            <div className='flex flex-col text-center items-center gap-2'>
+                <p className='font-mokoto text-[#00EBFE] text-xl lg:text-2xl'>CHAIRMAN</p>
+                <img  className='w-24 h-24 lg:w-48 lg:h-48 object-cover rounded-full' src={images[0]} alt='no'/>
+                <p className='font-medium text-xs lg:text-lg'>{data[0]["name"]}</p>
                     {/* <p className='italic font-light text-xs lg:text-sm'>{data[i]["position"]}</p> */}
-                </div>)}
-        
-                </>
-
+            </div>
+        </div>
+        <div className='grid grid-cols-2 lg:grid-cols-4 font-poppins gap-y-10'>
+        {images.slice(1, images.length-1).map((img, i) => {
+            return(
+                <div key={img} className='flex flex-col text-center items-center gap-2'>
+                    <img  className='w-24 h-24 lg:w-40 lg:h-40 object-cover rounded-full' src={img} alt='no'/>
+                    <p className='font-medium text-xs lg:text-lg'>{data[i+1]["name"]}</p>
+                    {/* <p className='italic font-light text-xs lg:text-sm'>{data[i]["position"]}</p> */}
+                </div>
             )
         })}
+        </div>
+        <div className='grid grid-cols-1 font-poppins mt-10'>
+          <div className='flex flex-col text-center items-center gap-2'>
+                <img  className='w-24 h-24 lg:w-40 lg:h-40 object-cover rounded-full' src={images[images.length-1]} alt='no'/>
+                <p className='font-medium text-xs lg:text-lg'>{data[images.length-1]["name"]}</p>
+                {/* <p className='italic font-light text-xs lg:text-sm'>{data[i]["position"]}</p> */}
+          </div>
         </div>
       </div>
     </div>
